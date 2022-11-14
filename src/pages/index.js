eae3998a9aa3db4805fa7b2b3600d4a9926f175b
rajahwu <vmradford@gmail.com>
2022-11-14 17:46:59 -0500

    add link components to home and about pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 const IndexPage = () => (
   <main>
-    <h1>Welcom to my Gatsby site!</h1>
+    <h1>Welcome to my Gatsby site!</h1>
+    <Link to="/about">About</Link>
     <p>I'm making this by following the Gatsby Tutorial.</p>
   </main>
 )
