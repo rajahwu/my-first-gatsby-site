a3247eb74cc760ddd2d2ebaf1a8ebf79ccb33a06
rajahwu <vmradford@gmail.com>
2022-11-17 14:51:28 -0500

    Revert "Finished Gatsby Tutorial Part 3"
    This reverts commit 3b3d7b72b627441c4c7656b2459837dcb37e28a7.
    
    qa!

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,5 @@ module.exports = {
     title: `My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-  ],
+  plugins: [],
 }
