b27cd0665e91941469547db9115ca56be948d2d5
rajahwu <vmradford@gmail.com>
2022-11-18 06:20:02 -0500

    add blog page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import * as React from 'react';
-import Layout from '../components/layout';
+import Layout  from '../components/layout';
 import { StaticImage } from 'gatsby-plugin-image';
+import Seo from '../components/seo';
 
 const IndexPage = () => (
   <Layout pageTitle="Home Page">
     <p>I'm making this by folowing the Gatsby Tutorial.</p>
-    <StaticImage 
-    alt="smilie"
-    src="../images/smilie.png"
+    <StaticImage
+      alt="smilie"
+      src="../images/smile.png"
     />
   </Layout>
 )
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
 export default IndexPage
